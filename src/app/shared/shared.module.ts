fc1328e0abebdfca24339f0a80243802b2608500
Ignacio Perez Roca <ignacio@Ignacios-MacBook-Pro.local>
2019-06-11 11:15:41 -0300

    Angular Material

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// App Modules
+import { MaterialModule } from '../material/material.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MaterialModule
+  ]
+})
+export class SharedModule { }
