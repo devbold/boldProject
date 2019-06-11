c73a10f089de0782a022436d36f0005e691f66c9
Ignacio Perez Roca <ignacio@Ignacios-MacBook-Pro.local>
2019-06-10 22:13:07 -0300

    Components and Modules

---
+++ src/app/views/views.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+// App Components
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipies/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+@NgModule({
+  declarations: [
+    RecipiesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    RecipiesComponent
+  ]
+})
+export class ViewsModule { }
